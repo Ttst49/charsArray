29799883801d7ce60f98a2335a77e773e6ef601b
Ttst <thibautstachnick@gmail.com>
2024-02-18 12:39:15 +0100

    add first function for charArray learning

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int firstCharArray(){
+    char tt[20];
+    tt[0] = 'B';
+    tt[1] = 'O';
+    tt[2] = 'N';
+    tt[3] = 'J';
+    tt[4] = 'O';
+    tt[5] = 'U';
+    tt[6] = 'R';
+    tt[7] = '\0';
+    cout << tt;
+    return 0;
+}
+
+int main() {
+    firstCharArray();
+    return 0;
+}
